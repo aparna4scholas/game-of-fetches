65a22b522abc7b84277ab6a4cd3dc7d594f40801
aparna4scholas <aparna4scholas@gmail.com>
2020-04-01 11:34:27 -0700

    modified

--- game-of-thrones/src/App.js
+++ game-of-thrones/src/App.js
@@ -1,10 +1,57 @@
 import React, { Component } from 'react'
+import './App.css';
+import axios from 'axios'
 
 export default class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isLoaded: false,
+      data: []
+    }
+  }
+
+  componentDidMount() {
+    // |||||||||||||| fetch |||||||||||||||
+    // fetch("https://api.spoonacular.com/recipes/search?query=cheese&number=4&apiKey=27a02bbb5b48401f96bfda6a7d3e2545")
+    //   .then(response => response.json()) // first response => resolved
+    //   .then(
+    //     result => {
+    //       console.log(result.results)
+
+    //       this.setState({
+    //         isLoaded: true,
+    //         items:result.results
+    //       })
+
+    //       console.log("state is updated",this.state.data)
+    //     }
+    //   ).catch(e => console.log("there's a error", e))
+    // |||||||||||||||||||||||||||||||||||||
+
+    axios.get("https://anapioficeandfire.com/api/characters/583")
+    .then(res => {
+      const book = res.data.results;
+      console.log("book ---->", book)
+      
+      this.setState({data: book})
+
+    })
+    .catch(error => {
+      console.log('there is an error', error)
+    })
+
+  }
+
+
   render() {
     return (
       <div>
-        hi
+        <h1>  </h1>
+        <ul>
+          {this.state.data.map((book, id) => <li key={id}> {book.title} </li>)}
+        </ul>
       </div>
     )
   }
